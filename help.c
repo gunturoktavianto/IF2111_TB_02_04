a5053df866a14a6ad94b3bf6ceef718c3c775693
Valchri <124064355+Valchri@users.noreply.github.com>
2023-10-31 09:04:24 +0700

    bagian help

--- help.c
+++ help.c
@@ -13,7 +13,7 @@ void SessionHelp() {
     printf("3. QUEUE -> Untuk memanipulasi queue lagu yang memiliki 5 tipe command, yaitu SONG, PLAYLIST, SWAP, REMOVE, dan CLEAR\n");
     printf("4. SONG -> Untuk navigasi lagu yang ada pada queue lagu saat ini\n");
     printf("5. PLAYLIST -> Untuk melakukan basic command untuk playlist yaitu CREATE, ADD, SWAP, REMOVE dan DELETE\n");
-    printf("6. STATUS -> Untuk menampilkan lagu yang sedang dimainkan beserta Queue song yang ada dan dari playlist mana lagu itu diputar");
+    printf("6. STATUS -> Untuk menampilkan lagu yang sedang dimainkan beserta Queue song yang ada dan dari playlist mana lagu itu diputar\n");
     printf("7. SAVE -> Untuk menyimpan state ke dalam file\n");
     printf("8. QUIT -> Untuk keluar dari sesi\n");
 }
