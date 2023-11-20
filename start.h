5bbe24ed459aa3c50131e60c942a509f152e7678
khansadilla <117127484+khansadilla@users.noreply.github.com>
2023-11-20 11:52:55 +0700

    insha Allah aman

---
+++ start.h
@@ -0,0 +1,15 @@
+#ifndef start_H
+#define start_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "ADT/mesinkata/mesinkata.c"
+#include "ADT/mesinkata/mesinkarakter.c"
+#include "ADT_BUATAN/ALBUM/album.c"
+#include "ADT_BUATAN/PENYANYI/penyanyi.c"
+#include "ADT_BUATAN/LAGU/lagu.c"
+
+void startconfig();
+
+#endif
